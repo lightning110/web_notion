bf74dfa4e345bc4a77b5d46134696b835904aa0d
lightning <admin@chenxyz.cn>
2023-11-06 01:06:47 +0800

    v1.83

--- themes/plog/components/Modal.js
+++ themes/plog/components/Modal.js
@@ -118,7 +118,7 @@ export default function Modal(props) {
                                         )}    
                                     </div>
                                     <div className='z-10 absolute hover:opacity-50 opacity-0 duration-200 transition-opacity w-full top-0 left-0 px-4 h-full items-center flex justify-between'>
-                                    <a class="box" href={`${BLOG.SUB_PATH}/${modalContent.slug}`}><div class="box"></div></a>
+                                    <Link class="box" href={`${BLOG.SUB_PATH}/${modalContent.slug}`}><div class="box"></div></Link>
                                         <div class="inbox_pos left" onClick={prev}><ChevronLeft className='cursor-pointer w-24 h-32 hover:opacity-100 stroke-white stroke-1 scale-y-150' /></div>
                                         <div class="inbox_pos right" onClick={next}><ChevronRight className='cursor-pointer w-24 h-32 hover:opacity-100 stroke-white stroke-1 scale-y-150' /></div>
                                       {/* <div class="inbox_pos left" onClick={prev}></div>
